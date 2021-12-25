58126a40e7c51ff9c8e426f774c9026d095f5115
kim <yuyukgh@naver.com>
2021-12-25 17:40:29 +0900

    feat: 리스트 info데이터 업데이트

--- src/components/List.js
+++ src/components/List.js
@@ -39,20 +39,32 @@ function List() {
     <div className={cx('container')}>
       <p className={cx('total')}>전체 {total}개</p>
       <ul className={cx('photos')}>
-        {photos?.map((item) => (
-          <li key={item.id}>
-            <div
-              className={cx('photo')}
-              style={{ backgroundImage: `url(${item.photoUrl})` }}
-            >
-              <p className={cx('result')}>미완료</p>
-            </div>
-            <div className={cx('info')}>
-              <h3 className={cx('title')}>파일 이름</h3>
-              <p className={cx('label')}>라벨 8개</p>
-            </div>
-          </li>
-        ))}
+        {photos?.map((item) => {
+          const splitCut = item.photoUrl.split('/');
+          const title = splitCut[splitCut.length - 1].split('.');
+          console.log(item.labels);
+          return (
+            <li key={item.id}>
+              <div
+                className={cx('photo')}
+                style={{ backgroundImage: `url(${item.photoUrl})` }}
+              >
+                <p
+                  className={cx([
+                    'result',
+                    { sucess: item.labels.length === 0 },
+                  ])}
+                >
+                  {item.labels.length === 0 ? '완료' : '미완료'}
+                </p>
+              </div>
+              <div className={cx('info')}>
+                <h3 className={cx('title')}>파일 이름 : {title[0]}</h3>
+                <p className={cx('label')}>라벨 {item.labels.length}개</p>
+              </div>
+            </li>
+          );
+        })}
       </ul>
 
       <Pagination
