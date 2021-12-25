718504ddc74c565fef9e8c3b30e5d2306f006e64
kim <yuyukgh@naver.com>
2021-12-25 17:57:12 +0900

    style: app 사이즈 중앙정렬

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import classNames from 'classnames/bind';
+
+import styles from './App.module.scss';
+
 import List from './components/List';
 
+const cx = classNames.bind(styles);
+
 function App() {
-  return <List />;
+  return (
+    <div className={cx('wrapper')}>
+      <List />
+    </div>
+  );
 }
 
 export default App;
