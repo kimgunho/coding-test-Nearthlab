feb2e08fc74dcbd8a5e9d6151549c72896e84884
kim <yuyukgh@naver.com>
2021-12-25 19:19:10 +0900

    feat: nav 컴포넌트 구성 및 스타일링

--- src/components/list/Nav.js
+++ src/components/list/Nav.js
@@ -1,5 +1,38 @@
+import { useState, useEffect } from 'react';
+import classNames from 'classnames/bind';
+
+import styles from './Nav.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Nav() {
-  return <div>nav...</div>;
+  const [labels, setLabels] = useState([]);
+
+  useEffect(() => {
+    getLabels();
+  }, []);
+
+  const getLabels = async () => {
+    const url = 'https://tester-api.nearthlab.com/v1/labelTypes';
+    const response = await fetch(url);
+    const results = await response.json();
+    setLabels(results);
+  };
+  return (
+    <div className={cx('container')}>
+      <h2 className={cx('title')}>라벨 종류 선택</h2>
+      <form>
+        <ul className={cx('labels')}>
+          {labels?.map((label) => (
+            <li key={label.id}>
+              <input id={`label${label.id}`} type="checkbox" />
+              <label htmlFor={`label${label.id}`}>{label.title}</label>
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
 }
 
 export default Nav;
