c2117e72afd06deb709762d15f9691484de7e238
kim <yuyukgh@naver.com>
2021-12-25 17:51:16 +0900

    feat: 라벨상태에 따른 완료 미완료 구분

--- src/components/List.js
+++ src/components/List.js
@@ -42,7 +42,6 @@ function List() {
         {photos?.map((item) => {
           const splitCut = item.photoUrl.split('/');
           const title = splitCut[splitCut.length - 1].split('.');
-          console.log(item.labels);
           return (
             <li key={item.id}>
               <div
@@ -60,7 +59,14 @@ function List() {
               </div>
               <div className={cx('info')}>
                 <h3 className={cx('title')}>파일 이름 : {title[0]}</h3>
-                <p className={cx('label')}>라벨 {item.labels.length}개</p>
+                <p
+                  className={cx([
+                    'label',
+                    { hidden: item.labels.length === 0 },
+                  ])}
+                >
+                  라벨 {item.labels.length}개
+                </p>
               </div>
             </li>
           );
