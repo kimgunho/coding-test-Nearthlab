4969eaf281aeefe28e0670631b71e2886e52de89
kim <yuyukgh@naver.com>
2021-12-28 22:08:20 +0900

    style: 반응형 스타일링 업데이트

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -62,7 +62,7 @@ function Detail() {
         photoUrl: prev.photoUrl,
         photoTakenAt: prev.photoTakenAt,
         createdAt: prev.createdAt,
-        completed: !prev.completed,
+        completed: value,
         labels: prev.labels,
       };
     });
@@ -81,98 +81,102 @@ function Detail() {
   };
 
   return (
-    <div className={cx(['container', { on: detailActive }])}>
-      <div className={cx('contents')}>
-        <h2 className={cx('title')}>
-          파일 상세 정보{' '}
-          <button onClick={handleClearDetail} className={cx('close')}>
-            ✕
-          </button>
-        </h2>
-        {currentPhoto !== null ? (
-          <>
-            <div className={cx('inner')}>
-              <div className={cx('info')}>
-                <img src={currentPhoto?.photoUrl} alt="" />
-                <ul>
-                  <li>
-                    <span className={cx('title')}>파일명</span>
-                    <span className={cx('text')}>
-                      {currentPhoto?.photoUrl
-                        .split('/')
-                        .map((line, index) =>
-                          index === currentPhoto?.photoUrl.split('/').length - 1
-                            ? line.replace(/\.jpg/g, '')
-                            : '',
+    <>
+      <div className={cx(['container', { on: detailActive }])}>
+        <div className={cx('contents')}>
+          <h2 className={cx('title')}>
+            파일 상세 정보{' '}
+            <button onClick={handleClearDetail} className={cx('close')}>
+              ✕
+            </button>
+          </h2>
+          {currentPhoto !== null ? (
+            <>
+              <div className={cx('inner')}>
+                <div className={cx('info')}>
+                  <img src={currentPhoto?.photoUrl} alt="" />
+                  <ul>
+                    <li>
+                      <span className={cx('title')}>파일명</span>
+                      <span className={cx('text')}>
+                        {currentPhoto?.photoUrl
+                          .split('/')
+                          .map((line, index) =>
+                            index ===
+                            currentPhoto?.photoUrl.split('/').length - 1
+                              ? line.replace(/\.jpg/g, '')
+                              : '',
+                          )}
+                      </span>
+                    </li>
+                    <li>
+                      <span className={cx('title')}>촬영시간</span>
+                      <span className={cx('text')}>
+                        {moment(currentPhoto?.photoTakenAt).format(
+                          'YYYY.MM.DD a HH:mm:ss',
                         )}
-                    </span>
-                  </li>
-                  <li>
-                    <span className={cx('title')}>촬영시간</span>
-                    <span className={cx('text')}>
-                      {moment(currentPhoto?.photoTakenAt).format(
-                        'YYYY.MM.DD a HH:mm:ss',
-                      )}
-                    </span>
-                  </li>
-                  <li>
-                    <span className={cx('title')}>등록일</span>
-                    <span className={cx('text')}>
-                      {moment(currentPhoto?.createdAt).format('YYYY.MM.DD')}
-                    </span>
-                  </li>
-                </ul>
-              </div>
-              <div className={cx('labels')}>
-                <h3 className={cx('title')}>라벨 정보</h3>
-                <ul>
-                  {currentPhoto?.labels.length === 0 ? (
-                    <li className={cx('notLabel')}>
-                      라벨이 존재하지 않습니다.
+                      </span>
                     </li>
-                  ) : (
-                    ''
-                  )}
-                  {currentPhoto?.labels.map((label, index) => (
-                    <li key={label.id}>
-                      <h4 className={cx('title')}>라벨 #{index + 1}</h4>
-                      <ul className={cx('info')}>
-                        <li>
-                          <h5 className={cx('subTitle')}>유형</h5>
-                          <p className={cx('text')}>
-                            {checkLabels(label.typeId)}
-                          </p>
-                        </li>
-                        <li>
-                          <h5 className={cx('subTitle')}>설명</h5>
-                          <p className={cx('text')}>{label.description}</p>
-                        </li>
-                      </ul>
+                    <li>
+                      <span className={cx('title')}>등록일</span>
+                      <span className={cx('text')}>
+                        {moment(currentPhoto?.createdAt).format('YYYY.MM.DD')}
+                      </span>
                     </li>
-                  ))}
-                </ul>
-                <button
-                  onClick={() =>
-                    handleChangePhoto(
-                      currentPhoto?.id,
-                      !currentPhoto?.completed,
-                    )
-                  }
-                  className={cx([
-                    'button',
-                    !currentPhoto?.completed ? 'on' : 'off',
-                  ])}
-                >
-                  {!currentPhoto?.completed ? '완료' : '미완료'}
-                </button>
+                  </ul>
+                </div>
+                <div className={cx('labels')}>
+                  <h3 className={cx('title')}>라벨 정보</h3>
+                  <ul>
+                    {currentPhoto?.labels.length === 0 ? (
+                      <li className={cx('notLabel')}>
+                        라벨이 존재하지 않습니다.
+                      </li>
+                    ) : (
+                      ''
+                    )}
+                    {currentPhoto?.labels.map((label, index) => (
+                      <li key={label.id}>
+                        <h4 className={cx('title')}>라벨 #{index + 1}</h4>
+                        <ul className={cx('info')}>
+                          <li>
+                            <h5 className={cx('subTitle')}>유형</h5>
+                            <p className={cx('text')}>
+                              {checkLabels(label.typeId)}
+                            </p>
+                          </li>
+                          <li>
+                            <h5 className={cx('subTitle')}>설명</h5>
+                            <p className={cx('text')}>{label.description}</p>
+                          </li>
+                        </ul>
+                      </li>
+                    ))}
+                  </ul>
+                  <button
+                    onClick={() =>
+                      handleChangePhoto(
+                        currentPhoto?.id,
+                        !currentPhoto?.completed,
+                      )
+                    }
+                    className={cx([
+                      'button',
+                      !currentPhoto?.completed ? 'on' : 'off',
+                    ])}
+                  >
+                    {!currentPhoto?.completed ? '완료' : '미완료'}
+                  </button>
+                </div>
               </div>
-            </div>
-          </>
-        ) : (
-          <div>loading...</div>
-        )}
+            </>
+          ) : (
+            <div>loading...</div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className={cx(['dimmed', { on: detailActive }])} />
+    </>
   );
 }
 
